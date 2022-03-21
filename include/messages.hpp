9f4e76b6a47c65802bda2f23a787835db86f3e5d
NOUR <nourbennourdj@gmail.com>
2022-03-22 00:06:39 +0100

    Modification of messages.hpp.

--- include/messages.hpp
+++ include/messages.hpp
@@ -12,14 +12,14 @@
 
 typedef struct s2c
 {
-	int ID;
-	bool start;
+	int ID = -1;
+	bool start = false;
 } t_s2c;
 
 typedef struct c2s
 {
 	int ID;
-	bool Freq_sensor;
-	bool Areq_sensor;
-	bool Flevel_sensor;
+	bool Freq_sensor = false;
+	bool Areq_sensor = false;
+	bool Flevel_sensor = false;
 } t_c2s;
