85320b0f04d3f0e4ea426869bb3d08cd43ee08fe
NOUR <nourbennourdj@gmail.com>
2022-03-21 22:52:21 +0100

    Add my_debug.hpp Header.
    It enables debuging fonctions.

---
+++ include/my_debug.hpp
@@ -0,0 +1,53 @@
+// To ensure that the file will be included only once
+#pragma once
+
+#define DEBUG
+// Uncommit this to debug OnDataSent
+// #define ONDATASENT
+// Uncommit this to debug OnDataRecv
+// #define ONDATARECV
+// Uncommit this to debug SendReadings
+// #define SENDREADINGS
+// Uncommit this to debug updateDisplay
+#define UPDATEDISPLAY
+// Uncommit this to chose board ID 1
+#define BOARDID1
+// Debug session
+// Uncommit this to chose board ID 2
+// #define BOARDID2
+// Debug session
+#ifdef DEBUG
+#define D(x) x
+#else
+#define D(x)
+#endif
+#ifdef ONDATASENT
+#define DBG_ODS(x) D(x)
+#else
+#define DBG_ODS(x)
+#endif
+#ifdef ONDATARECV
+#define DBG_ODR(x) D(x)
+#else
+#define DBG_ODR(x)
+#endif
+#ifdef SENDREADINGS
+#define DBG_SR(x) D(x)
+#else
+#define DBG_SR(x)
+#endif
+#ifdef UPDATEDISPLAY
+#define DBG_UD(x) D(x)
+#else
+#define DBG_UD(x)
+#endif
+#ifdef BOARDID1
+#define DBG_BID1(x) D(x)
+#else
+#define DBG_BID1(x)
+#endif
+#ifdef BOARDID2
+#define DBG_BID2(x) D(x)
+#else
+#define DBG_BID2(x)
+#endif
