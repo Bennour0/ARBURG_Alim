022510f3a54247c5de0e4572d2d8cf3d31f7600a
NOUR <nourbennourdj@gmail.com>
2022-03-21 23:44:16 +0100

    Adding sending part.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,24 @@
 #include <WiFi.h>
 #include <esp_now.h>
 #include "messages.hpp"
+#include "my_debug.hpp"
+
+uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x62, 0x12, 0x98};
+typedef struct statistics
+{
+    int in = 0;
+    int out = 0;
+} t_stat;
+
+t_stat s;
+t_s2c s2c;
+t_c2s c2s;
 
 void initESPNOW();
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len);
+void SendReadings();
+void Send_task(void *parameter);
 
 void setup()
 {
@@ -32,7 +46,7 @@ void initESPNOW()
     // Register for a callback function that will be called when data is received
     esp_now_register_recv_cb(OnDataRecv);
 
-    /*esp_now_peer_info_t peerInfo;
+    esp_now_peer_info_t peerInfo;
     // Register peer
     memcpy(peerInfo.peer_addr, broadcastAddress, 6);
     peerInfo.channel = 0;
@@ -42,11 +56,22 @@ void initESPNOW()
     {
         Serial.println("Failed to add peer");
         return;
-    }*/
+    }
+    xTaskCreatePinnedToCore(
+        Send_task,   // Function to be called
+        "Send_task", // Name of task
+        5000,             // stack size (bytes in ESP32, words in FreeRTOS)
+        NULL,             // Parameter to pass to function
+        1,                // Task priority (0 to configMAX_PRIORITIES - 1)
+        NULL,             // Task handle
+        CONFIG_ARDUINO_RUNNING_CORE);
 }
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
+    DBG_ODS(Serial.print("Inside OnDataSent\n");)
+    s.out++;
+    DBG_ODS(Serial.print("Send messages = %d\n", s.out);)
     /*DBG_ODS(Serial.print("\r\nLast Packet Send Status:\t");)
     DBG_ODS(Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");)
     if (status == 0)
@@ -61,6 +86,9 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
+    DBG_ODR(Serial.print("Inside OnDataRecv\n");)
+    s.in++;
+    DBG_ODR(Serial.print("Received messages = %d\n", s.in);)
     /*DBG_ODR(char macStr[18];)
     DBG_ODR(Serial.print("Packet received from: ");)
     DBG_ODR(snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
@@ -70,3 +98,26 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
     holding_Data.id = Rec_Readings.id;
     holding_Data.stat_Cen = Rec_Readings.stat_Cen;*/
 }
+
+void SendReadings()
+{
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&c2s, sizeof(c2s));
+    if (result == ESP_OK)
+    {
+        DBG_SR(Serial.println("Sent with success");)
+    }
+    else
+    {
+        DBG_SR(Serial.println("Error sending the data");)
+    }
+}
+
+void Send_task(void *parameter)
+{
+    while (1)
+    {
+        SendReadings();
+        delay(1000);
+    }
+}    
