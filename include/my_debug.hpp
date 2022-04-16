86be82fb3502c65584d15660350c7cc919f59cce
R. ALOUI <aloui.radhouane@gmail.com>
2022-04-16 21:34:27 +0100

    Add server debug guards

--- include/my_debug.hpp
+++ include/my_debug.hpp
@@ -3,6 +3,7 @@
 
 #define MY_DEBUG
 
+#ifdef ESP32SERVER
 //#define D_PRINTMACADD
 //#define D_STARTESPNOW
 //#define D_ONDATASENT
@@ -51,39 +52,4 @@
 #else
   #define D(x)
 #endif
-
-/*
-// Uncommit this to debug OnDataSent
-// #define ONDATASENT
-// Uncommit this to debug OnDataRecv
-// #define ONDATARECV
-// Uncommit this to debug SendReadings
-// #define SENDREADINGS
-// Uncommit this to debug updateDisplay
-#define SHOWRECEIVINGS
-// Debug session
-#ifdef MY_DEBUG
-#define D(x) x
-#else
-#define D(x)
 #endif
-#ifdef ONDATASENT
-#define DBG_ODS(x) D(x)
-#else
-#define DBG_ODS(x)
-#endif
-#ifdef ONDATARECV
-#define DBG_ODR(x) D(x)
-#else
-#define DBG_ODR(x)
-#endif
-#ifdef SENDREADINGS
-#define DBG_SR(x) D(x)
-#else
-#define DBG_SR(x)
-#endif
-#ifdef SHOWRECEIVINGS
-#define DBG_SHR(x) D(x)
-#else
-#define DBG_SHR(x)
-#endif*/
