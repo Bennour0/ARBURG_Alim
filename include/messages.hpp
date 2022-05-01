bcd5dd2cdea0089268112797968694a6720a7d82
R. ALOUI <aloui.radhouane@gmail.com>
2022-05-01 14:43:44 +0100

    Add new types

--- include/messages.hpp
+++ include/messages.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
-typedef struct s2c
-{
+typedef struct{
+	int in = 0;
+	int out = 0;
+} t_inout;
+
+typedef struct s2c{
 	int ID = -1;
 	bool start = false;
 } t_s2c;
 
-typedef struct c2s
-{
+typedef struct c2s{
 	int ID;
 	bool Freq_sensor = false;
 	bool Areq_sensor = false;
 	bool Flevel_sensor = false;
+	t_inout inout;
 } t_c2s;
