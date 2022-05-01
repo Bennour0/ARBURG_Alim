8a7a63e2ff8010c1d8de6e3b1838d7374f9fb4a5
NOUR <nourbennourdj@gmail.com>
2022-05-01 17:13:01 +0100

    Update client with students config.

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -2,12 +2,18 @@
 
 /*(uint8_t id, uint8_t sen_feedmax_pin, uint8_t sen_arburg_pin, uint8_t sen_feedmax_lvl_pin,
           uint8_t vlv_charger_pin, uint8_t vlv_feedmax_pin, uint8_t vlv_drymax_pin)*/
-ClientP c(1, 2, 3, 4, 5, 6, 7);
+ClientP c(1, 27, 26, 25, 13, 12, 14);
 t_c2s m;
 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(27,INPUT);
+    pinMode(26,INPUT);
+    pinMode(25,INPUT);
+    pinMode(13,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(14,OUTPUT);
     Serial.println("Starting client program");
     c.startESPNOW();
     Serial.print("The client mac add : ");
