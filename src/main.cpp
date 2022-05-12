4151ce1061b4c73c5566fe6d9190b95efc25f3b6
R. ALOUI <aloui.radhouane@gmail.com>
2022-05-12 23:11:13 +0100

    Initial interruption working program
    The led is set to pin 2 so it is the internal led. The button is connected
    to pin 18.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <Arduino.h>
 
-
+#define Btn1_GPIO   18
+#define LED1_GPIO   2
+ 
+ int nbChanges = 0;
+void IRAM_ATTR Ext_INT1_ISR()
+{
+  // Toggle The LED
+  digitalWrite(LED1_GPIO, !digitalRead(LED1_GPIO));
+  Serial.printf("State changed %5d\n", ++nbChanges);
+}
+ 
 void setup()
 {
-    
+  Serial.begin(9600);
+  Serial.printf("Start setup\n");
+  pinMode(LED1_GPIO, OUTPUT);
+  pinMode(Btn1_GPIO, INPUT_PULLDOWN);
+  attachInterrupt(Btn1_GPIO, Ext_INT1_ISR, RISING);
+  Serial.printf("End setup\n");
 }
+ 
 void loop()
 {
-
+  // Do Nothing...
 }
