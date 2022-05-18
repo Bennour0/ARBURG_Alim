606822450c4a833972a8e739b92616db612ba6de
NOUR <nourbennourdj@gmail.com>
2022-05-18 23:13:36 +0100

    Add documentation to server code.

--- include/Server.hpp
+++ include/Server.hpp
@@ -4,7 +4,7 @@
 #include <ESP32Time.h>
 
 ESP32Time rtc;
-using namespace std;
+using std::queue;
 
 #ifdef ESP32
   #include <WiFi.h>
@@ -29,29 +29,135 @@ using namespace std;
 
 class ServerP{
 public:
+  /**
+   * @brief Construct a new Server P object
+   * 
+   * @param pump_pin Central pump pin number
+   * @param vlv1_pin Central pump valve one pin number
+   * @param vlv2_pin Central pump valve two pin number
+   * @param vlvr_pin Central tank valve pin number
+   */
   ServerP(uint8_t pump_pin, uint8_t vlv1_pin, uint8_t vlv2_pin, uint8_t vlvr_pin);
-  void printLocalMacAdd(); 
+  /**
+   * @brief function that returns local MAC address
+   * 
+   */
+  void printLocalMacAdd();
+  /**
+   * @brief function that initialize ESP NOW protocol
+   * 
+   */
   void startESPNOW();
+  /**
+   * @brief function that broadcast data to all system clients
+   * 
+   */
   void broadcast();
+  /**
+   * @brief function that sends data to client
+   * 
+   * @param mac client MAC address
+   * @param s2c struct to be sent
+   */
   void send2client(const uint8_t *mac, t_s2c s2c);
+  /**
+   * @brief function that checks server queue
+   * 
+   */
   void checkSQ();
+  /**
+   * @brief function that shows server queue
+   * 
+   * @param g queue to be shown
+   */
   static void showQ(queue<t_c2s> g);
+  /**
+   * @brief function that runs server
+   * 
+   */
   void runServer();
+  /**
+   * @brief function that initialize the system
+   * 
+   */
   void begin();
 private:
+  /**
+   * @brief function that setup server pins
+   * 
+   */
   void setupPins();
+   /**
+   * @brief function to be executed when a message is sent
+   * 
+   * @param mac_addr Mac address
+   * @param status Protocol status
+   */
   static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+  /**
+   * @brief function to be executed when a message is received
+   * 
+   * @param mac Mac addrees
+   * @param incomingData received message
+   * @param len received message length
+   */
   static void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len);
+  /**
+   * @brief function that prints MAC addresses
+   * 
+   * @param mac MAC address to be printed
+   */
   static void printMacAdd(const uint8_t * mac);
+  /**
+   * @brief functions that shows client informations
+   * 
+   * @param c2s client struct that contains the informations
+   */
   static void printClientInfo(const t_c2s *c2s);
+  /**
+   * @brief clients MAC addresses
+   * 
+   */
   uint8_t arburgMacAdd[NBARBURG][6] = {ADDMAC1, ADDMAC2};
+  /**
+   * @brief system client numbers
+   * 
+   */
   static t_c2s arburgs_data[NBARBURG];
+  /**
+   * @brief Central pump pin number
+   * 
+   */
   static uint8_t central_pump;
+  /**
+   * @brief Central pump valve one pin number
+   * 
+   */
   static uint8_t pump_vlv1;
+  /**
+   * @brief Central pump valve two pin number
+   * 
+   */
   static uint8_t pump_vlv2;
+  /**
+   * @brief Central tank valve pin number
+   * 
+   */
   static uint8_t tank_vlv;
+  /**
+   * @brief struct to be received from client
+   * 
+   */
   static t_c2s c2s;
+  /**
+   * @brief struct to be sent to client
+   * 
+   */
   static t_s2c s2c;
+  /**
+   * @brief counter struct that will count in and out data
+   * 
+   */
   static t_inout inout;
 };
 //uint8_t arburgMacAdd[][6] = {ADDMAC1, ADDMAC2};
